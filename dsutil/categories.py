70cf7d5ba2e20ae3aa6079eb3a8a3ab3d7da11f9
Felipe Almeida <queirozfcom@gmail.com>
2018-07-22 01:59:01 -0300

    minor commit (automatic message)

--- dsutil/categories.py
+++ dsutil/categories.py
@@ -7,33 +7,66 @@ class ToDummiesTransformer(BaseEstimator, TransformerMixin):
     """ Create dummies and drop the original column
     """
 
-    def __init__(self, columns=None):
+    def __init__(self, column_names=None, category_values=None):
+        """
 
-        if columns is None:
-            pass
-        elif type(columns) is not list:
-            columns = [columns]
-            
-        self.columns = columns
+        :param column_names: list of column names to extract dummies for.
+            if not given, all columns in the dataframe are used.
 
-    def transform(self, in_df, columns=None):
+        :param category_values: a dict mapping column names to all possible values
+            that categorical column can take. If not given, all values are used,
+            in whatever order they appear in the dataframe.
+
+        """
+
+        if column_names is None:
+            self.column_names = None
+        elif type(column_names) is not list:
+            self.column_names = [column_names]
+        else:
+            self.column_names = column_names
+
+        if category_values is not None and (not isinstance(category_values, dict)):
+            raise ValueError('category_values parameters must be None or a dict, got {}'.format(type(category_values)))
+        else:
+            self.category_values = category_values
+
+    def transform(self, in_df):
         """
 
         :param in_df:
-        :param columns:
         :return:
         """
 
-
-        if self.columns is None:
-            columns = in_df.columns.values
+        if self.column_names is None:
+            column_names = in_df.columns.values
         else:
-            columns = self.columns
+            column_names = self.column_names
 
-        cpy_df = in_df.copy()
+        if self.category_values is None:
+            cpy_df = in_df.copy()
+        else:
+            # user has provided category values so we must set those
+            cpy_df = in_df.copy()
 
-        for column_name in columns:
-            cpy_df = pd.concat([cpy_df, pd.get_dummies(cpy_df[column_name], prefix=column_name)], axis=1)
+            for column_name in self.column_names:
+
+                # maybe not all column_names have been set values
+                if self.category_values.get(column_name, None) is not None:
+
+                    provided_category_values = self.category_values[column_name]
+                    cpy_df[column_name] = cpy_df[column_name].astype(
+                        pd.api.types.CategoricalDtype(provided_category_values))
+                else:
+                    # user hasn't provided values, maybe the column is already category type?
+                    if isinstance(cpy_df[column_name].type, pd.api.types.CategoricalDtype):
+                        pass  # then leave it alone
+                    else:
+                        cpy_df[column_name] = cpy_df[column_name].astype(pd.api.types.CategoricalDtype())
+
+        # after all columns have been properly converted to categorical types, do the actual conversions
+        for column_name in column_names:
+            cpy_df = pd.concat([cpy_df, pd.get_dummies(cpy_df[column_name], prefix=column_name, dummy_na=True)], axis=1)
             cpy_df.drop([column_name], axis=1, inplace=True)
 
         return cpy_df
@@ -46,18 +79,28 @@ class SelectColumnsTransfomer(BaseEstimator, TransformerMixin):
     """ Select dataframe columns
     """
 
-    def __init__(self, columns=None):
+    def __init__(self, columns=None, ravel=None):
 
-        if type(columns) is not list:
-            columns = [columns]
-        elif columns is None:
-            columns = []
+        if columns is None:
+            self.columns = []
+        elif type(columns) is not list:
+            self.columns = [columns]
+        else:
+            self.columns = columns
 
-        self.columns = columns
+        if ravel is None:
+            self.ravel = False
+        else:
+            self.ravel = ravel
 
     def transform(self, X, **transform_params):
+
         cpy_df = X[self.columns].copy()
-        return cpy_df
+
+        if self.ravel:
+            return cpy_df.values.ravel()
+        else:
+            return cpy_df
 
     def fit(self, X, y=None, **fit_params):
         return self
--- dsutil/preprocessing.py
+++ dsutil/preprocessing.py
@@ -2,12 +2,13 @@
 
 import pandas as pd
 
-def encode_categorical_columns(in_df,categorical_column_names):
-    
+
+def encode_categorical_columns(in_df, categorical_column_names):
     out_df = in_df.copy()
-    
+
     for col_name in categorical_column_names:
         column = out_df[col_name].astype(pd.api.types.CategoricalDtype())
-        out_df = pd.concat([out_df,pd.get_dummies(column, prefix=col_name,dummy_na=True)],axis=1).drop([col_name],axis=1)
-        
+        out_df = pd.concat([out_df, pd.get_dummies(column, prefix=col_name, dummy_na=True)], axis=1).drop([col_name],
+                                                                                                          axis=1)
+
     return out_df
